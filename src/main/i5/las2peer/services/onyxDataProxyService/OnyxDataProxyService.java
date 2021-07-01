464d848cd92eab27e55834ea057d22231cd2f2e5
Philipp Dolif <git.phil.cd@gmail.com>
2021-07-01 09:22:54 +0200

    Update service version to v1.1.0

--- src/main/i5/las2peer/services/onyxDataProxyService/OnyxDataProxyService.java
+++ src/main/i5/las2peer/services/onyxDataProxyService/OnyxDataProxyService.java
@@ -65,7 +65,7 @@ import io.swagger.annotations.SwaggerDefinition;
 @SwaggerDefinition(
 		info = @Info(
 				title = "Onyx Data Proxy Service",
-				version = "1.0.1",
+				version = "1.1.0",
 				description = "A proxy for onyx data",
 				contact = @Contact(
 						name = "Alexander Tobias Neumann",
